303d08c1070163cf1fd86cca8cb378a73104ccde
Pradeep-2564 <ppradeep4525@gmail.com>
2025-03-19 14:08:10 +0530

    adding luxury section

--- taj-website/src/Components/Hotels/Hotels.jsx
+++ taj-website/src/Components/Hotels/Hotels.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hotels = () => {
   return (
     <div>
-      
+      <img src="https://cdn.sanity.io/images/ocl5w36p/prod3/d0804c623c7fa3863fb7c330e484bc533a8363d6-3840x1320.jpg?w=768&auto=format&dpr=2" alt="img" />
     </div>
   )
 }
